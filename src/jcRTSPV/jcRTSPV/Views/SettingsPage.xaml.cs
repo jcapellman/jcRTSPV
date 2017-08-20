3f4a28e2c186b8bd66a55e27125f8d2e76549219
Jarred Capellman <jcapellman@hotmail.com>
2017-08-19 21:49:44 -0500

    Added: MessageDialog messages on I/O operations

--- src/jcRTSPV/jcRTSPV/Views/SettingsPage.xaml.cs
+++ src/jcRTSPV/jcRTSPV/Views/SettingsPage.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -21,9 +22,11 @@ namespace jcRTSPV.Views
             ViewModel.LoadSettings();
         }
 
-        private void btnSave_OnClick(object sender, RoutedEventArgs e)
+        private async void btnSave_OnClick(object sender, RoutedEventArgs e)
         {
             ViewModel.WriteSettings();
+
+            await new MessageDialog("Settings Saved").ShowAsync();
         }
 
         private void btnCancel_OnClick(object sender, RoutedEventArgs e)
@@ -48,9 +51,15 @@ namespace jcRTSPV.Views
             pNewForm.IsOpen = true;
         }
 
-        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        private async void btnDelete_Click(object sender, RoutedEventArgs e)
         {
+            var count = lvCameraFeeds.SelectedItems.Cast<string>().Count();
+
             ViewModel.RemoveSelectedFeeds(lvCameraFeeds.SelectedItems.Cast<string>().ToList());
+
+            var md = new MessageDialog($"{count} feed(s) removed");
+
+            await md.ShowAsync();
         }
 
         private void lvCameraFeeds_SelectionChanged(object sender, SelectionChangedEventArgs e)
