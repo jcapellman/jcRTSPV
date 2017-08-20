a3fc0788edbc36cae38c2c04dab62e7e4c7122a5
Jarred Capellman <jcapellman@hotmail.com>
2017-08-20 10:23:59 -0500

    Change: Refactored to utilize a BasePage class

---
+++ src/jcRTSPV/jcRTSPV/Views/BasePage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+
+namespace jcRTSPV.Views
+{
+    public class BasePage : Page
+    {
+        public async void ShowMessageBox(string content)
+        {
+            var md = new MessageDialog(content);
+
+            await md.ShowAsync();
+        }
+    }
+}
--- src/jcRTSPV/jcRTSPV/Views/SettingsPage.xaml.cs
+++ src/jcRTSPV/jcRTSPV/Views/SettingsPage.xaml.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 
-using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -9,24 +7,24 @@ using jcRTSPV.ViewModels;
 
 namespace jcRTSPV.Views
 {
-    public sealed partial class SettingsPage : Page
+    public sealed partial class SettingsPage : BasePage
     {
         private SettingsViewModel ViewModel => (SettingsViewModel) DataContext;
 
         public SettingsPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
 
             DataContext = new SettingsViewModel();
 
             ViewModel.LoadSettings();
         }
 
-        private async void btnSave_OnClick(object sender, RoutedEventArgs e)
+        private void btnSave_OnClick(object sender, RoutedEventArgs e)
         {
             ViewModel.WriteSettings();
 
-            await new MessageDialog(Common.Constants.MSG_SETTINGS_SAVED).ShowAsync();
+            ShowMessageBox(Common.Constants.MSG_SETTINGS_SAVED);
         }
 
         private void btnCancel_OnClick(object sender, RoutedEventArgs e)
@@ -34,13 +32,13 @@ namespace jcRTSPV.Views
             pNewForm.IsOpen = false;
         }
 
-        private async void btnAdd_OnClick(object sender, RoutedEventArgs e)
+        private void btnAdd_OnClick(object sender, RoutedEventArgs e)
         {
             var result = ViewModel.AddFeed();
 
             if (!result)
             {
-                await new MessageDialog(Common.Constants.MSG_SETTINGS_INVALID_URL).ShowAsync();
+                ShowMessageBox(Common.Constants.MSG_SETTINGS_INVALID_URL);
 
                 return;
             }
@@ -58,15 +56,13 @@ namespace jcRTSPV.Views
             pNewForm.IsOpen = true;
         }
 
-        private async void btnDelete_Click(object sender, RoutedEventArgs e)
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
             var count = lvCameraFeeds.SelectedItems.Cast<string>().Count();
 
             ViewModel.RemoveSelectedFeeds(lvCameraFeeds.SelectedItems.Cast<string>().ToList());
 
-            var md = new MessageDialog($"{count} {Common.Constants.MSG_SETTINGS_INVALID_URL}");
-
-            await md.ShowAsync();
+            ShowMessageBox($"{count} {Common.Constants.MSG_SETTINGS_INVALID_URL}");            
         }
 
         private void lvCameraFeeds_SelectionChanged(object sender, SelectionChangedEventArgs e)
